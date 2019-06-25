c9475ffc01b75ad92d000bd308d749b1a61fc951
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-25 14:28:26 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards (names , day) {
+  let cards = []
+for ( let i = 0; i < names.length ; i++ ) {
+  cards.push(`Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+  }
+  return cards
+}
+
+function countdown (num) {
+  while (num > -1) {
+    console.log(num);
+    num -= 1;
+  } 
+}
