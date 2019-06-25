1646e1608be29127e8663f52ad86ff6e3519ed8f
newtypeV2 <gianlibunao@gmail.com>
2019-06-25 11:56:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, message){
+    let thankYouArray = [];
+     for(let i=0;i<array.length;i++){
+        thankYouArray.push(`Thank you, ${array[i]}, for the wonderful ${message} gift!`);
+    }
+    return thankYouArray;
+}
+
+function countdown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
