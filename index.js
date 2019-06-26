be4e9e6d3eed530c729464acd9abbc1fb7a60e89
Your Name <you@example.com>
2019-06-25 21:27:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let arr = [];
+
+function writeCards(array, event) {
+for (let i = 0; i < array.length ;i++ ) {
+arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+}
+return arr
+}
+
+function countdown(int) {
+
+  while (int >= 0 ) {
+    console.log(int)
+    int--
+  }
+}
